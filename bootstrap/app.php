2d79a3000c8e08af6aa583b72037904519fc58b9
DiegoRamosSanchez <diegoramossanchez2004@gmail.com>
2026-06-16 09:52:19 -0500

    LOW : FORWARD PORT

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
+
+        // Para funcionamiento en redirecciones de puertos
+        # $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
